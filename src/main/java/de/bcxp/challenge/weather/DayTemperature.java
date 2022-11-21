5402299fb226decc2e45df96ea35ab9f709dbf63
geczinorbert <geczinorbi97@gmail.com>
2022-11-21 18:49:47 +0200

    Created Weather data model

---
+++ src/main/java/de/bcxp/challenge/weather/DayTemperature.java
@@ -0,0 +1,14 @@
+package de.bcxp.challenge.weather;
+
+public class DayTemperature {
+
+    private int dayOfTheMonth;
+
+    private int maximumTemperature ;
+
+    private int minimumTemperature;
+
+    public int getTemperatureDifference(){
+        return this.maximumTemperature - this.minimumTemperature;
+    }
+}
