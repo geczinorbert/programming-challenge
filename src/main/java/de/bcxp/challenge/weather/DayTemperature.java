316afffa913814ed934fadcf76867dc8d25c31f6
geczinorbert <geczinorbi97@gmail.com>
2022-11-21 20:08:25 +0200

    Added equals override for testing

--- src/main/java/de/bcxp/challenge/weather/DayTemperature.java
+++ src/main/java/de/bcxp/challenge/weather/DayTemperature.java
@@ -1,10 +1,11 @@
 package de.bcxp.challenge.weather;
 
+import java.util.Objects;
+
 /**
  * Bean to represent a Day with min and max temperatures
  */
 public class DayTemperature {
-
     private int dayOfTheMonth;
 
     private int maximumTemperature;
@@ -23,5 +24,16 @@ public class DayTemperature {
 
     public int getTemperatureDifference() {
         return this.maximumTemperature - this.minimumTemperature;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DayTemperature that = (DayTemperature) o;
+        return dayOfTheMonth == that.dayOfTheMonth && maximumTemperature == that.maximumTemperature && minimumTemperature == that.minimumTemperature;
+    }    @Override
+    public int hashCode() {
+        return Objects.hash(dayOfTheMonth, maximumTemperature, minimumTemperature);
     }
 }
