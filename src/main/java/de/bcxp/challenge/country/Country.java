109033e42d66c48b4ee8e3640ac5c5dbe383aa2d
geczinorbert <geczinorbi97@gmail.com>
2022-11-21 20:28:52 +0200

    Added country class

---
+++ src/main/java/de/bcxp/challenge/country/Country.java
@@ -0,0 +1,31 @@
+package de.bcxp.challenge.country;
+
+import java.util.Objects;
+
+public class Country {
+
+    private String name;
+
+    private double population;
+
+    private double area;
+
+    public Country(String name, double population, double area) {
+        this.name = name;
+        this.population = population;
+        this.area = area;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Country country = (Country) o;
+        return Double.compare(country.population, population) == 0 && Double.compare(country.area, area) == 0 && Objects.equals(name, country.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, population, area);
+    }
+}
